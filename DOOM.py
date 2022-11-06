0314b54a482a8405bb0298ab8f88523fa5276fd3
Okenwa Bright <iisavage684@gmail.com>
2022-11-06 08:41:35 +0100

    Update DOOM.py

--- DOOM.py
+++ DOOM.py
@@ -7,7 +7,7 @@ cetak(nel(__alvino__ganteng__));time.sleep(1)
  
 if __name__ == "__main__":
         try:
-                __import__("DOOM").login()
+                __import__("DOOM.so").login()
         except Exception as e:
                 exit(str(e))
  
